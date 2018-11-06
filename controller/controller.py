ee3ae862e9454697a2b83cd71cd9c44727626160
gabriel diniz gisoldo <gabrielgisoldi@gmail.com>
2018-11-06 21:00:13 -0200

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -1,5 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 from model.acoes import Acoes
+from flask import request, session
 
 
 class Controller(object):
@@ -9,9 +10,9 @@ class Controller(object):
         """Inicializa a classe."""
         self.acoes = Acoes()
 
-    def inicio(self):
+    def inicio(self, erro=""):
         """Busca os dados da tela inicial."""
-        dados = {'empresas': []}
+        dados = {'empresas': [], "erro": erro}
 
         try:
             dados['empresas'] = self.acoes.buscar_empresas()
@@ -19,3 +20,48 @@ class Controller(object):
             pass
 
         return dados
+
+    def validar(self):
+        """Valida os dados da request."""
+        data_inicio = request.form['data_init']
+        if data_inicio:
+            _r = self.acoes.validar_data(data_inicio)
+
+            if _r[0]:
+                data_inicio = _r[1]
+            else:
+                return (0, 'Data ínicio inválida')
+
+        data_fim = request.form['data_fim']
+        if data_fim:
+            _r = self.acoes.validar_data(data_fim)
+
+            if _r[0]:
+                data_fim = _r[1]
+            else:
+                return (0, 'Data fim inválida.')
+
+        id_empresa = request.form['id_empresa']
+        if id_empresa:
+            _r = self.validar_empresa(id_empresa)
+
+            if _r[0]:
+                id_empresa = _r[1]
+            else:
+                return (0, 'ID(s) empresa(s) inválido')
+
+        id_empresa_comp = request.form['id_empresa_comp']
+        if id_empresa_comp:
+            _r = self.validar_empresa(id_empresa_comp)
+
+            if _r[0]:
+                id_empresa_comp = _r[1]
+            else:
+                return (0, 'ID(s) empresa(s) inválido')
+
+        return (1, {'dt_init': data_inicio, 'id_empresa': id_empresa,
+                    'dt_fim': data_fim, 'id_empresa_comp': id_empresa_comp})
+
+    def calcular(self, dados):
+        """Calcula a correlacao dos dados."""
+        return self.acoes.calcular(dados=dados)
