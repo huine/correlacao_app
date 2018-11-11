abc4cdbec81ff10d74109fe6af3643b2afd1cebc
gabriel diniz gisoldo <gabrielgisoldi@gmail.com>
2018-11-10 23:59:54 -0200

    Update controller/controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -23,7 +23,7 @@ class Controller(object):
 
     def validar(self):
         """Valida os dados da request."""
-        return request.get_json(force=True)
+        return request.form
         data_inicio = request.form.get('data_init', '')
         if data_inicio:
             _r = self.acoes.validar_data(data_inicio)
